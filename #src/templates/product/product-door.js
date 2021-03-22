9dd3e2e24d3175e0527200e7121f6b20cdf1e9b3
Poltoratchi <globusglobusfff@gmail.com>
2021-03-23 00:30:29 +0200

    added fix cardF and new pages (cardF-glass, cardF-twodoor)

--- #src/templates/product/product-door.js
+++ #src/templates/product/product-door.js
@@ -1,8 +1,11 @@
 $(function () {
- // global variables
+ // глобальные переменные
  const body = $('body');
  const scrollWidth = $(window).outerWidth() - $(window).width();
  const mediaWidth = $(window).width();
+
+
+ // плагины старт
  //инициализируем галерею ДО запуска слайдера
  const gallery = $('.slider__for-item .fancybox-link');
  //при клике на ссылку в слайде запускаем галерею
@@ -30,8 +33,7 @@ $(function () {
  });
 
 
-
-
+// слайдеры карточка товара
  if (mediaWidth < 1200) {
   $('.product__slider-for').slick({
    slidesToShow: 1,
@@ -40,23 +42,31 @@ $(function () {
    fade: true,
   });
  } else {
-  $('.product__slider-for').slick({
-   slidesToShow: 1,
-   slidesToScroll: 1,
-   arrows: false,
-   fade: true,
-   asNavFor: '.product__slider-nav'
-  });
-
-  $('.product__slider-nav').slick({
-   slidesToShow: 5,
-   slidesToScroll: 1,
-   asNavFor: '.product__slider-for',
-   dots: false,
-   centerMode: true,
-   focusOnSelect: true,
-   vertical: true
-  })
+  if($('.product').hasClass('product-onyx')) {
+   $('.product__slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+   });
+  } else {
+   $('.product__slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.product__slider-nav'
+   });
+ 
+   $('.product__slider-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.product__slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    vertical: true
+   })
+  }
  }
  $(".rating-block").rateYo({
   rating: 3.5,
@@ -65,13 +75,16 @@ $(function () {
   ratedFill: "#fe8a61",
   spacing: "2px",
  });
+ // comment fancybox
+ $('[data-fancybox="gallery"]').fancybox({
+  loop: true,
+  keyboard: true,
+ });
 
 
+// плагины конец
 
-
-
-
- // comment preview photo (5)
+ // фото на превью в комментарии (5)
  const itemsCont = $('.product__comment-images').children();
  const items = document.querySelectorAll('.comment-img-item');
  const counterCreate = document.createElement('span');
@@ -89,31 +102,30 @@ $(function () {
   });
  }
 
- // comment fancybox
- $('[data-fancybox="gallery"]').fancybox({
-  loop: true,
-  keyboard: true,
- });
-
  //product icons (color, glazing)
  body.on('click', '.product-icon', function () {
   productIconChoise($(this));
  });
 
- // product card set choice
+ // выбор комплекта (смена цены и названия)
  body.on('click', '.choice-set', function () {
   const currentSet = $(this).text();
-  const newTitle = $(this).parents('.product__choose').find('.current-set');
-  if ($(this).hasClass('without-text')) {
-   $('#without-text').show();
-  } else {
-   newTitle.text(`В комплекте: ${currentSet}`);
-  }
+  const newTitle = $(this).parents('.product__choose').find('#change-text-set');
+  const startText = $(this).parents('.product__choose').find('.without-text').text()
+  let counterPrice = $(this).parents('.product__choose').find('.price-counter-sum');
+  const setPrice = +$(this).attr('data-price');
+  const totalPrice = +counterPrice.attr('data-start-price')
 
+  if ($(this).hasClass('without-text')) {
+   newTitle.text();
+   counterPrice.text(counterPrice.attr('data-start-price'))
+  } else {
+   newTitle.text(`${currentSet}`);
+   counterPrice.text((totalPrice + setPrice))
+  }
  });
 
-
- //popyp product preview
+ // класс открытия popup
  body.on('click', '.modal-link', function () {
   const thisBtn = $(this).attr('data-modal');
   const thisName = $(this).find('span').text();
@@ -133,6 +145,7 @@ $(function () {
   }
  });
 
+ // класс закрытия popup
  body.on('click', '.close-btn', function () {
   $(this).parents('.modal-block').removeClass('active');
   if (!$('.popup-additional__wrapper').hasClass('active')) {
@@ -142,42 +155,40 @@ $(function () {
   }
  });
 
+// показываем элементы скрытые функцией showSomeItems
  body.on('click', '.show-all-btn', function () {
   const elem = $(this).parents('.hidden-body-simple').find('div[data-show="hidden"]');
   const text = $(this).text();
 
-  elem.toggleClass('hidden-elem-simple');
+  elem.toggle();
   $(this).text(text == 'Показать всё' ? 'Скрыть' : 'Показать всё');
  });
 
-
- //closes blocks
+ // закрытие блоков нажатием за пределы
  $(document).on('mouseup', function (e) {
   const modal = body.find('.modal-block');
   const additionalContent = body.find('.additional-popup');
   const additionalModal = body.find('.popup-additional__wrapper');
   const addAdditionalModal = body.find('.add-addittional-popup');
-  const scrollBlock = $('#scroll-row');
   let axisPositionX = e.pageX;
 
-  // вывод результата в консоль
-  // close modalPreview
+  // закрытиеmodalPreview
   if (modal.hasClass('active') && !modal.is(e.target) && modal.has(e.target).length === 0) {
    modal.removeClass('active');
    body.removeClass('smoke').css({
     'padding-right': '0px'
    });
   }
-  // close scroll popup additional
+  // закрытие scroll popup additional
   if (addAdditionalModal.hasClass('active')) {
-   // закрывает добавление в корзину в попапе с доп элементами
+   // закрытие добавление в корзину в попапе с доп элементами
    if (!addAdditionalModal.is(e.target) && addAdditionalModal.has(e.target).length === 0) {
     addAdditionalModal.removeClass('active')
    }
   } else if (additionalModal.hasClass('active')) {
-   // закрывает попап с просмотром доп эементов
+   // закрытие попап с просмотром доп эементов
    if (!additionalContent.is(e.target) && additionalContent.has(e.target).length === 0 && axisPositionX < mediaWidth - scrollWidth) {
-    // console.log(axisPositionX)
+
     additionalModal.removeClass('active');
     body.removeClass('smoke').css({
      'padding-right': '0px'
@@ -187,21 +198,19 @@ $(function () {
  });
 
 
-
-
+ // функции вызова
  const showSomeItems = (num, el) => {
-  el.forEach((elem, i) => {
+  el.each((i, elem) => {
    if (i > num) {
-    elem.setAttribute('data-show', 'hidden');
-    elem.classList.add('hidden-elem-simple')
+    $(elem).hide().addClass('hidden-elem-simple').attr('data-show', 'hidden');
    }
   })
  }
+ // скрываем ненужные элементы и открываем их по кнопке '.show-all-btn'
+ showSomeItems(1, $('.product__table-item'));
+ showSomeItems(7, $('.product__characteristics-row'));
 
- showSomeItems(1, document.querySelectorAll('.product__table-item'));
- showSomeItems(7, document.querySelectorAll('.product__characteristics-row'));
-
-
+ // переключатель current класса дочерних элементов
  const productIconChoise = (elem) => {
   elem.addClass('current').siblings().removeClass('current');
  }
