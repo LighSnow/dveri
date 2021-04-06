97481fd0f4b0a6783e6be06569efcd9863d61ee1
Serghei <lighsnow@gmail.com>
2021-04-06 22:56:08 +0300

    added simplebar

--- #src/js/components/scroll.js
+++ #src/js/components/scroll.js
@@ -1,41 +1,53 @@
 (function ($) {
-  function customScroll() {
-    $('.product-comparison__inner').mCustomScrollbar({
-      theme: "dark-3",
-      axis: 'x',
-      mouseWheel: { axis: "x" },
-      scrollInertia: 1000,
-      documentTouchScroll: true,
-      callbacks: {
-        whileScrolling: function () {
-          let abs = Math.abs(this.mcs.left);
-          let position = $('.pos');
-          position.css('left', abs);
-          console.log($('.pos'));
-        }
-      }
-    });
-  }
+  // function customScroll() {
+  //   $('.product-comparison__inner').mCustomScrollbar({
+  //     theme: "dark-3",
+  //     axis: 'x',
+  //     mouseWheel: { axis: "x" },
+  //     scrollInertia: 1000,
+  //     documentTouchScroll: true,
+  //     callbacks: {
+  //       whileScrolling: function () {
+  //         let abs = Math.abs(this.mcs.left);
+  //         let position = $('.pos');
+  //         position.css('left', abs);
+  //         console.log($('.pos'));
+  //       }
+  //     }
+  //   });
+  // }
 
   const width = $(window).width();
   $(window).on("load resize scroll", function () {
     const parent = $('.body-product');
     if (parent.find('.scroll').length > 0) {
-        $(".scroll").mCustomScrollbar({
-          theme: "dark-3"
-        });
+      $(".scroll").mCustomScrollbar({
+        theme: "dark-3"
+      });
     } else {
       $(".scroll").mCustomScrollbar({
         theme: "dark-3"
       });
     }
 
-    customScroll()
+    // customScroll()
 
     // $(window).on('resize', function () {
     // customScroll();
     // });
-    console.log($(document).width());
+
   });
 
+  // let simpleScroll = document.querySelectorAll('.select');
+
+  // simpleScroll.forEach(item => {
+  //   item = new SimpleBar();
+  // })
+
+
+
 })(jQuery);
+
+
+// new SimpleBar(document.getElementById('myElement'));
+const simpleBar = new SimpleBar(document.getElementById('myElement'), { autoHide: false });
